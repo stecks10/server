85981e09932e8e5588aec4a3dcc91996fe1f386f
Vitor Nunes do Nascimento <stecks10@gmail.com>
2025-02-04 10:31:20 -0300

    Rota de Perfil

---
+++ src/http/routes/get-profile.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import { authenticateUserHook } from "../hooks/authenticate-user";
+import { getUser } from "../../functions/get-user";
+
+export const getProfileRoute: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    "/profile",
+    {
+      onRequest: [authenticateUserHook],
+      schema: {
+        tags: ["auth"],
+        description: "Get authenticated user's profile",
+        response: {
+          201: z.object({
+            profile: z.object({
+              id: z.string(),
+              name: z.string().nullable(),
+              email: z.string().nullable(),
+              avatarUrl: z.string().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, replay) => {
+      const userId = request.user.sub;
+
+      const { user } = await getUser({
+        userId,
+      });
+
+      return replay.status(200).send({ profile: user });
+    }
+  );
+};
--- src/http/server.ts
+++ src/http/server.ts
@@ -16,6 +16,7 @@ import { fastifyJwt } from "fastify-jwt";
 
 import { authenticateFromGithubRoute } from "./routes/authenticate-from-github";
 import { env } from "../env";
+import { getProfileRoute } from "./routes/get-profile";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -48,6 +49,7 @@ app.register(createCompletionRoute);
 app.register(getPendingGoalsRoute);
 app.register(getWeekSummaryRoute);
 app.register(authenticateFromGithubRoute);
+app.register(getProfileRoute);
 
 app
   .listen({
